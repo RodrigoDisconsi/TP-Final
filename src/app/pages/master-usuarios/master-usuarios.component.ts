ec953a92db313892504085b5b5f1a03d14931b84
Rodrigo <48496310+RodrigoDisconsi@users.noreply.github.com>
2022-06-16 18:01:25 -0300

    cambio de iconos y agrego para descargar excel en master-usuarios

--- src/app/pages/master-usuarios/master-usuarios.component.ts
+++ src/app/pages/master-usuarios/master-usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserInterface } from 'src/app/models/UserInterface';
 import { FirebaseService } from 'src/app/servicios/firebase-service.service';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-master-usuarios',
@@ -23,27 +24,25 @@ export class MasterUsuariosComponent implements OnInit {
     this.nuevoUsuario = true;
   }
 
-  // exportExcel() {
-  //   import("xlsx").then(xlsx => {
-  //       const worksheet = xlsx.utils.json_to_sheet(this.sales); // Sale Data
-  //       const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-  //       const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
-  //       this.saveAsExcelFile(excelBuffer, "sales");
-  //   });
-  // }
-  // saveAsExcelFile(buffer: any, fileName: string): void {
-  //   import("file-saver").then(FileSaver => {
-  //     let EXCEL_TYPE =
-  //       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-  //     let EXCEL_EXTENSION = ".xlsx";
-  //     const data: Blob = new Blob([buffer], {
-  //       type: EXCEL_TYPE
-  //     });
-  //     FileSaver.saveAs(
-  //       data,
-  //       fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
-  //     );
-  //   });
-  // }
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.users); // Sale Data
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "users");
+    });
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+      let EXCEL_TYPE =
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+      let EXCEL_EXTENSION = ".xlsx";
+      const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE
+      });
+      FileSaver.saveAs(
+        data,
+        fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+      );
+  }
 
 }
